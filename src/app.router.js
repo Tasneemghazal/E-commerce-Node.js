3c319fa1fe4be3cca90e6d561e720d369b4e95f3
Tasneem <tasneemgazal5@gmail.com>
2024-04-28 13:20:22 +0300

    use cors in app router

--- src/app.router.js
+++ src/app.router.js
@@ -2,9 +2,10 @@ import connectDB from "../DB/connection.js";
 import categoryRouter from "./modules/category/category.router.js";
 import productRouter from "./modules/product/product.router.js";
 import userRouter from "./modules/user/user.router.js";
-
+import cors from 'cors'
 const initApp = (app,express)=>{
     connectDB();
+    app.use(cors());
     app.use(express.json());
     app.get('/', (req, res)=>{
         return res.status(200).json({message:"success"});
