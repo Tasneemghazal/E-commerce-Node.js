8d7c07664516b3ef4d7d16ff594d23d773cf76d2
Tasneem <tasneemgazal5@gmail.com>
2024-04-28 15:08:58 +0300

    create add category function and add its router

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,3 +1,23 @@
+import slugify from "slugify";
+import categoryModel from "../../../DB/models/category.model.js";
+import cloudinary from "../../utils/cloudinary.js"
+export const addCategory = async(req,res,next) => {
+   req.body.name = req.body.name.toLowerCase();
+    if(await categoryModel.findOne({name: req.body.name})){
+        return res.status(409).json({message:"category already exists"});
+    }
+    
+    const {secure_url,public_id}= await cloudinary.uploader.upload(req.file.path,{
+        folder:'ghazalShop/category',
+    })
+    req.body.slug = slugify(req.body.name);
+    req.body.image ={
+        secure_url,
+        public_id
+    }
+    const category = await categoryModel.create(req.body)
+    return res.status(201).json({message:"success",category})
+}
 export const getAllCategories = (req,res,next) =>{
     return res.json("categories");
 }
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as Categories from "./category.controller.js";
+import uploadFile, { fileTypes } from '../../utils/multer.js'
 const categoryRouter = Router({caseSensitive: true});
-
+categoryRouter.post('/addCategory',uploadFile(fileTypes.image).single('image'),Categories.addCategory);
 categoryRouter.get('/getAll',Categories.getAllCategories);
 export default categoryRouter;
