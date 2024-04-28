704f27802df371f6ab2ba722edb2fa5560bfbca1
Tasneem <tasneemgazal5@gmail.com>
2024-04-28 13:47:46 +0300

    create category model

---
+++ DB/models/category.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const categorySchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        min:4,
+        max:20,
+    },
+    image:{
+        type:Object,
+        required:true,
+    },
+    slug:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        default:'active',
+        enum:['active','inactive']
+    },
+    createdBy:{type:Types.ObjectId,ref:'User',required:true},
+    updatedBy:{type:Types.ObjectId,ref:'User',required:true},
+
+},{
+    timestamps:true,
+});
+
+const categoryModel = model('Category',categorySchema);
+export default categoryModel;
