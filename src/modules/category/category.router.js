7d7509fa141732aed48f1e7890b432c3cefbc962
Tasneem <tasneemgazal5@gmail.com>
2024-05-05 11:22:54 +0300

    add auth middleware to create and update category routes

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as Categories from "./category.controller.js";
 import uploadFile, { fileTypes } from '../../utils/multer.js'
+import { auth } from "../../middlewares/auth.middleware.js";
 const categoryRouter = Router({caseSensitive: true});
-categoryRouter.post('/addCategory',uploadFile(fileTypes.image).single('image'),Categories.addCategory);
+categoryRouter.post('/addCategory',auth(),uploadFile(fileTypes.image).single('image'),Categories.addCategory);
 categoryRouter.get('/getAll',Categories.getAllCategories);
 categoryRouter.get('/active',Categories.getActive);
 categoryRouter.get('/:id',Categories.getDetails);
-categoryRouter.patch('/:id',uploadFile(fileTypes.image).single('image'),Categories.update);
+categoryRouter.patch('/:id',auth(),uploadFile(fileTypes.image).single('image'),Categories.update);
 categoryRouter.delete('/:id',Categories.destory);
 export default categoryRouter;
