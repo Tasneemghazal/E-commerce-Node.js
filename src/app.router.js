7430adaa02f305fc67dd3dd4480830e3fb5c869c
Tasneem <tasneemgazal5@gmail.com>
2024-05-08 14:28:42 +0300

    add subCategory Router

--- src/app.router.js
+++ src/app.router.js
@@ -2,6 +2,7 @@ import connectDB from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
 import categoryRouter from "./modules/category/category.router.js";
 import productRouter from "./modules/product/product.router.js";
+import subCategoryRouter from "./modules/subCategory/subCategory.router.js";
 import userRouter from "./modules/user/user.router.js";
 import cors from 'cors'
 const initApp = (app,express)=>{
@@ -12,6 +13,7 @@ const initApp = (app,express)=>{
         return res.status(200).json({message:"success"});
     })
     app.use('/category',categoryRouter);
+    app.use('/subcategory',subCategoryRouter);
     app.use('/product',productRouter);
     app.use('/auth',authRouter);
     app.use('/user',userRouter);
