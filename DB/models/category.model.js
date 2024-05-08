9c96b99f49ff0899a413850f510fe920f841d513
Tasneem <tasneemgazal5@gmail.com>
2024-05-08 14:28:12 +0300

    add virtual attribute to category model

--- DB/models/category.model.js
+++ DB/models/category.model.js
@@ -25,7 +25,15 @@ const categorySchema = new Schema({
 
 },{
     timestamps:true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
 });
+
+categorySchema.virtual("subcategory", {
+    localField:'_id',
+    foreignField: 'categoryId',
+    ref: 'SubCategory'
+})
 
 const categoryModel = model('Category',categorySchema);
 export default categoryModel;
