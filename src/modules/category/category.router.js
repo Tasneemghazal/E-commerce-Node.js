554c9e13d73c75fc2018ced462da224ea45cdf8d
Tasneem <tasneemgazal5@gmail.com>
2024-05-08 14:29:32 +0300

    add subCategory router

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import * as Categories from "./category.controller.js";
 import uploadFile, { fileTypes } from '../../utils/multer.js'
 import { auth } from "../../middlewares/auth.middleware.js";
+import subCategoryRouter from "../subCategory/subCategory.router.js";
 const categoryRouter = Router({caseSensitive: true});
+categoryRouter.use('/:id/subcategory',subCategoryRouter)
 categoryRouter.post('/addCategory',auth(),uploadFile(fileTypes.image).single('image'),Categories.addCategory);
 categoryRouter.get('/getAll',Categories.getAllCategories);
 categoryRouter.get('/active',Categories.getActive);
