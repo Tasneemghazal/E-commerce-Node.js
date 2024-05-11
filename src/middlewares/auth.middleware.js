0ae278f4e2d2fb15062c98fbf0f495ea2d8415aa
Tasneem <tasneemgazal5@gmail.com>
2024-05-11 07:24:11 +0300

    add role authorization in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import userModel from "../../DB/models/user.model.js";
 
-export const auth = () => {
+export const auth = (accessRole=[]) => {
   return async (req, res, next) => {
     const {authorization} = req.headers;
     if (!authorization?.startsWith(process.env.BEARERTOKEN)) {
@@ -12,8 +12,11 @@ export const auth = () => {
     if (!decoded) {
       return res.status(400).json({ message: "invalid token" });
     }
-    const user = await userModel.findById(decoded.id).select("userName");
+    const user = await userModel.findById(decoded.id).select("userName role");
     if (!user) return res.status(404).json({ message: "user not found" });
+    if(!accessRole.includes(user.role)){
+      return res.status(403).json({ message: "not authorized" });
+    }
     req.user = user;
     next();
   };
