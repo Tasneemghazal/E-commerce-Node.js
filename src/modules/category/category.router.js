17e61a4c2b2c61e09a5042e3eea0ab31bc232500
Tasneem <tasneemgazal5@gmail.com>
2024-05-11 07:26:06 +0300

    edit auth middleware in endPoints to accept role

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -3,12 +3,14 @@ import * as Categories from "./category.controller.js";
 import uploadFile, { fileTypes } from '../../utils/multer.js'
 import { auth } from "../../middlewares/auth.middleware.js";
 import subCategoryRouter from "../subCategory/subCategory.router.js";
+import { endPoints } from "./category.role.js";
+
 const categoryRouter = Router({caseSensitive: true});
 categoryRouter.use('/:id/subcategory',subCategoryRouter)
-categoryRouter.post('/addCategory',auth(),uploadFile(fileTypes.image).single('image'),Categories.addCategory);
-categoryRouter.get('/getAll',Categories.getAllCategories);
+categoryRouter.post('/addCategory',auth(endPoints.create),uploadFile(fileTypes.image).single('image'),Categories.addCategory);
+categoryRouter.get('/getAll',auth(endPoints.get),Categories.getAllCategories);
 categoryRouter.get('/active',Categories.getActive);
 categoryRouter.get('/:id',Categories.getDetails);
-categoryRouter.patch('/:id',auth(),uploadFile(fileTypes.image).single('image'),Categories.update);
-categoryRouter.delete('/:id',Categories.destory);
+categoryRouter.patch('/:id',auth(endPoints.delete),uploadFile(fileTypes.image).single('image'),Categories.update);
+categoryRouter.delete('/:id',auth(endPoints.delete),Categories.destory);
 export default categoryRouter;
