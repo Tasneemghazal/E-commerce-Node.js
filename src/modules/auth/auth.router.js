25a44c72f0fb9dc978cc6feb6f809d0c14897390
Tasneem <tasneemgazal5@gmail.com>
2024-05-11 07:25:02 +0300

    add sendCode and ForgetPassword Routers

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -3,4 +3,6 @@ const authRouter = Router();
 import * as User from "./auth.controller.js"
 authRouter.post('/register',User.register)
 authRouter.post('/login',User.login)
+authRouter.patch('/send',User.sendCode)
+authRouter.patch('/forget',User.forgetPassword)
 export default authRouter;
