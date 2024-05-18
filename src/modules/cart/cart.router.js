f8012c91cbc9c001b7e5636bb7aaabe9e346f27e
Tasneem <tasneemgazal5@gmail.com>
2024-05-18 06:36:57 +0300

    craete caer router and product router

---
+++ src/modules/cart/cart.router.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as Cart from "./cart.controller.js";
+import { auth } from "../../middlewares/auth.middleware.js";
+import { endPoints } from "./cart.role.js";
+
+const cartRouter = Router({caseSensitive: true});
+
+cartRouter.post('/create',auth(endPoints.create),Cart.create);
+cartRouter.patch('/delete/:productId',auth(endPoints.delete),Cart.remove);
+cartRouter.patch('/clear',auth(endPoints.delete),Cart.clear);
+cartRouter.get('/getAll',auth(endPoints.create),Cart.getAll);
+cartRouter.patch('/increase/:productId',auth(endPoints.create),Cart.increase);
+cartRouter.patch('/decrease/:productId',auth(endPoints.create),Cart.decrease);
+export default cartRouter;
--- src/modules/product/product.router.js
+++ src/modules/product/product.router.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import * as Products from "./product.controller.js";
+import uploadFile, { fileTypes } from "../../utils/multer.js";
+import { auth } from "../../middlewares/auth.middleware.js";
+import { endPoints } from "./product.role.js";
 const productRouter = Router();
 
-productRouter.get('/',Products.getAllProducts);
+productRouter.post(
+  "/create",auth(endPoints.create),
+  uploadFile(fileTypes.image).fields([
+    { name: "mainImage", maxCount: 1 },
+    { name: "subImages", maxCount: 5 },
+  ]),
+  Products.create
+);
 export default productRouter;
