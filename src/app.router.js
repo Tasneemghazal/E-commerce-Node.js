d57c1f6964a35c4fb876f04fc2c094f1753fdbf9
Tasneem <tasneemgazal5@gmail.com>
2024-06-03 08:40:38 +0300

    add coupon , order and review routes

--- src/app.router.js
+++ src/app.router.js
@@ -2,6 +2,8 @@ import connectDB from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
 import cartRouter from "./modules/cart/cart.router.js";
 import categoryRouter from "./modules/category/category.router.js";
+import couponRouter from "./modules/coupon/coupon.router.js";
+import orderRouter from "./modules/order/order.router.js";
 import productRouter from "./modules/product/product.router.js";
 import subCategoryRouter from "./modules/subCategory/subCategory.router.js";
 import userRouter from "./modules/user/user.router.js";
@@ -19,7 +21,9 @@ const initApp = (app,express)=>{
     app.use('/auth',authRouter);
     app.use('/user',userRouter);
     app.use('/cart',cartRouter);
-    
+    app.use('/coupon',couponRouter);
+    app.use('/order',orderRouter);
+    app.use('/review',orderRouter);
     app.use('*', (req, res)=>{
         return res.status(404).json({message:"page not found"});
     })
