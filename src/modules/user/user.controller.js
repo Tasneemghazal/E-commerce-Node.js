43e24c4ba4c74d0b46074418a48b49159247758a
Tasneem <tasneemgazal5@gmail.com>
2024-06-03 08:44:54 +0300

    add get user

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,3 +1,17 @@
-export const getUser = (req, res,next) => {
-    return res.json("user");
+import userModel from "../../../DB/models/user.model.js"
+
+export const getUsers = async(req, res,next) => {
+    const users = await userModel.find();
+    if(!users){
+        return res.status(404).json({message: "No users found"});
+    }
+    return res.status(200).json({message:"success", users})
+}
+
+export const getUserData = async(req, res,next) => {
+    const user = await userModel.findById(req.user._id);
+    if(!user){
+        return res.status(404).json({message: "No user found"});
+    }
+    return res.status(200).json({message:"success", user})
 }
