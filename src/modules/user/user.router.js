2b9fdb89be2c0c9d840a61b773ade3cd35ab32fa
Tasneem <tasneemgazal5@gmail.com>
2024-06-03 08:45:32 +0300

    add getuser router

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import * as Users from "./user.controller.js"
+import { auth } from "../../middlewares/auth.middleware.js";
+import { endPoints } from "./user.role.js";
 const userRouter = Router();
- userRouter.get('/',Users.getUser);
+userRouter.get('/',auth(endPoints.getUsers),Users.getUsers);
+userRouter.get('/userData',auth(endPoints.getUserData),Users.getUserData);
 
 export default userRouter;
