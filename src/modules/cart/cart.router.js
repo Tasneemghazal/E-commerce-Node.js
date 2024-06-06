995a2ad76d43cf145453a1783d5a8cf257883b2c
Tasneem <tasneemgazal5@gmail.com>
2024-06-06 11:42:41 +0300

    add validation for add to cart

--- src/modules/cart/cart.router.js
+++ src/modules/cart/cart.router.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import * as Cart from "./cart.controller.js";
 import { auth } from "../../middlewares/auth.middleware.js";
 import { endPoints } from "./cart.role.js";
+import { validation } from "../../middlewares/validation.js";
+import { createCartSchema } from "./cart.validation.js";
 
 const cartRouter = Router({caseSensitive: true});
 
-cartRouter.post('/create',auth(endPoints.create),Cart.create);
+cartRouter.post('/create',auth(endPoints.create),validation(createCartSchema),Cart.create);
 cartRouter.patch('/delete/:productId',auth(endPoints.delete),Cart.remove);
 cartRouter.patch('/clear',auth(endPoints.delete),Cart.clear);
 cartRouter.get('/getAll',auth(endPoints.create),Cart.getAll);
