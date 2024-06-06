d70e685fd5e1cf9bd2393901e2f2bd22681427ad
Tasneem <tasneemgazal5@gmail.com>
2024-06-06 11:42:27 +0300

    add validation for register and login

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 const authRouter = Router();
 import * as User from "./auth.controller.js"
 import { checkEmail } from "../../middlewares/checkEmail.middleWare.js";
-authRouter.post('/register',checkEmail,User.register)
-authRouter.post('/login',User.login)
+import { asyncHandler } from "../../utils/errorHandler.js";
+import { validation } from "../../middlewares/validation.js";
+import { loginSchema, registerSchema } from "./auth.validation.js";
+authRouter.post('/register',validation(registerSchema),checkEmail,asyncHandler(User.register))
+authRouter.post('/login',validation(loginSchema),User.login)
 authRouter.patch('/send',User.sendCode)
 authRouter.patch('/forget',User.forgetPassword)
+authRouter.get('/confirmEmail/:token',User.confirmEmail)
 export default authRouter;
---
+++ src/modules/auth/auth.validation.js
@@ -0,0 +1,13 @@
+import Joi from 'joi'
+
+ export const registerSchema =Joi.object({
+    userName: Joi.string().min(4).max(30).required(),
+    email:Joi.string().email().required(),
+    password:Joi.string().pattern(/^[A-Z][a-z0-9]{3,20}$/),
+    
+});
+export const loginSchema =Joi.object({
+    email:Joi.string().email().required(),
+    password:Joi.string().pattern(/^[A-Z][a-z0-9]{3,20}$/),
+    
+});
