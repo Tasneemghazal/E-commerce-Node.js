5b1b0177febd9db6055bb3130c84a08a473c2965
Tasneem <tasneemgazal5@gmail.com>
2024-06-13 09:31:57 +0300

    handle validation to accept data from req.files and req.file

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,18 +1,31 @@
-import { registerSchema } from "../modules/auth/auth.validation.js"
+import { registerSchema } from "../modules/auth/auth.validation.js";
 
-export const validation = (schema)=>{
-
-    return (req,res,next)=>{
-        const errorsMsg=[];
-    const {error}= schema.validate(req.body,{abortEarly:false})
-    if(error){
-        error.details.forEach(err => {
-            const key =err.context.key;
-            errorsMsg.push({[key]:err.message});
-        });
-        return res.status(400).json({message:"validation error",errors:errorsMsg})
+export const validation = (schema) => {
+  return (req, res, next) => {
+    const errorsMsg = [];
+    let filterData = {};
+    if (req.file) {
+      filterData = {
+        image: req.file,
+        ...req.body,
+        ...req.params,
+        ...req.query,
+      };
+    } else if (req.files) {
+      filterData = { ...req.files, ...req.body, ...req.params, ...req.query };
+    } else {
+      filterData = { ...req.body, ...req.params, ...req.query };
     }
-    next()
+    const { error } = schema.validate(filterData, { abortEarly: false });
+    if (error) {
+      error.details.forEach((err) => {
+        const key = err.context.key;
+        errorsMsg.push({ [key]: err.message });
+      });
+      return res
+        .status(400)
+        .json({ message: "validation error", errors: errorsMsg });
     }
-
-}
+    next();
+  };
+};
