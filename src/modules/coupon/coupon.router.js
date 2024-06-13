650797afed634f41a4ab22ca28b4b40488b54a5b
Tasneem <tasneemgazal5@gmail.com>
2024-06-13 09:33:34 +0300

    add validation on create coupon

--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import * as Coupon from "./coupon.controller.js";
 import { auth } from "../../middlewares/auth.middleware.js";
 import { endPoints } from "./coupon.role.js";
+import { validation } from "../../middlewares/validation.js";
+import { createCouponSchema } from "./coupon.validation.js";
 const couponRouter = Router();
-couponRouter.post('/create',auth(endPoints.create),Coupon.create);
+couponRouter.post('/create',validation(createCouponSchema),auth(endPoints.create),Coupon.create);
 
 export default couponRouter;
