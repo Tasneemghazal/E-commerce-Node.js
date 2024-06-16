acbc078aab958e53487c343df60083656e6ce221
Tasneem <tasneemgazal5@gmail.com>
2024-06-16 11:45:17 +0300

    add addUserExcel router

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -5,8 +5,10 @@ import { checkEmail } from "../../middlewares/checkEmail.middleWare.js";
 import { asyncHandler } from "../../utils/errorHandler.js";
 import { validation } from "../../middlewares/validation.js";
 import { loginSchema, registerSchema, sendCodeSchema } from "./auth.validation.js";
+import uploadFile, { fileTypes } from "../../utils/multer.js";
 authRouter.post('/register',validation(registerSchema),checkEmail,asyncHandler(User.register))
-authRouter.post('/login',validation(loginSchema),User.login)
+authRouter.post('/Excel', uploadFile(fileTypes.excel).single('excel'), asyncHandler(User.addUserExcel));
+authRouter.post('/login',User.login)
 authRouter.patch('/send',validation(sendCodeSchema),User.sendCode)
 authRouter.patch('/forget',User.forgetPassword)
 authRouter.get('/confirmEmail/:token',User.confirmEmail)
